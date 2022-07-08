c81b3269533426c7fe70b987211edae105e9d853
lietava-git <lietava.jan@gmail.com>
2022-07-08 23:24:47 +0100

    - added event shipment generation sql script and added test

---
+++ engine/event_shipment_refresh.sql
@@ -0,0 +1,39 @@
+WITH inserted_shipment_event AS (
+INSERT INTO event_shipment (shipment_id, event_id, created_at)
+SELECT
+	lead_shipment.id AS shipment_id,
+	ev.id AS event_id,
+	timezone('utc', now()) as created_at
+	FROM
+	(SELECT
+			shipment.id,
+			departure.ship_imo,
+			departure.date_utc AS departure_date,
+			arrival.date_utc AS arrival_date,
+			shipment.status,
+			lead(departure.date_utc) over (
+				partition BY departure.ship_imo
+				ORDER BY departure.date_utc
+			) AS next_departure_date_utc
+	FROM shipment
+	LEFT JOIN departure ON departure.id = shipment.departure_id
+	LEFT JOIN arrival ON arrival.id = shipment.arrival_id
+	WHERE shipment.status = 'completed'
+	ORDER BY departure.ship_imo, departure.date_utc ASC) AS lead_shipment
+ 	LEFT JOIN (SELECT event.id, event.ship_imo, event.date_utc
+		   FROM event
+		   WHERE event.type_id = '21') AS ev
+		   ON ((ev.date_utc BETWEEN lead_shipment.departure_date AND lead_shipment.next_departure_date_utc)
+		   AND ev.ship_imo = lead_shipment.ship_imo)
+    ON CONFLICT (shipment_id)
+        DO UPDATE SET
+            shipment_id = EXCLUDED.shipment_id,
+            event_id = EXCLUDED.event_id,
+            created_at = timezone('utc', now())
+        RETURNING
+            shipment_id,
+            event_id,
+            created_at
+)
+SELECT count(event_id)
+FROM inserted_shipment_event;
