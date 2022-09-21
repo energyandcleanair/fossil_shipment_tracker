1891d3fa7588796f8f9ac5d6d976158b490fc206
Hubert Thieriot <hubert@energyandcleanair.org>
2022-09-21 19:03:08 +0200

    -first version working, with anomaly index

--- api/routes/flaring.py
+++ api/routes/flaring.py
@@ -2,20 +2,123 @@ import json
 import pandas as pd
 import datetime as dt
 import numpy as np
-
-
+import geopandas as gpd
+from shapely import wkb
+from sqlalchemy.sql import text
 from flask import Response
 from flask_restx import Resource, reqparse, inputs
 from base.models import Flaring, FlaringFacility
 from base.encoder import JsonEncoder
-from base.db import session
-from base.utils import to_datetime, to_list
+from base.db import session, engine
+from base.utils import to_datetime, to_list, update_geometry_from_wkb
 
 from . import routes_api
 
 
+@routes_api.route('/v0/flaring_facility', strict_slashes=False)
+class FlaringFacilityResource(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('facility_id', help='facility id(s)',
+                        type=str, action='split',
+                        default=None, required=False)
+    parser.add_argument('add_anomaly_index', help='Add an anomaly index to detect anomaly after 2022-02-24',
+                        type=inputs.boolean, default=True)
+    parser.add_argument('nest_in_data', help='Whether to nest the geojson content in a data key.',
+                        type=inputs.boolean, default=True)
+    parser.add_argument('format', type=str, help='format of returned results (json or csv)',
+                        required=False, default="json")
+    parser.add_argument('download', help='Whether to return results as a file or not.',
+                        type=inputs.boolean, default=False)
+
+    @routes_api.expect(parser)
+    def get(self):
+
+        params = FlaringFacilityResource.parser.parse_args()
+        facility_id = params.get("facility_id")
+        add_anomaly_index = params.get("add_anomaly_index")
+        format = params.get("format")
+        nest_in_data = params.get("nest_in_data")
+        download = params.get("download")
+
+        if add_anomaly_index:
+
+            with open('engine/flaring_anomaly_index.sql', 'r') as file:
+                sql_content = file.read()
+
+            with engine.connect() as con:
+                s = text(sql_content)
+                rs = con.execute(s, **{"facility_id": to_list([int(x) for x in facility_id]) if facility_id else None})
+
+            result = [r for r in rs]
+            result = pd.DataFrame(result)
+            result.columns = ["id", "name", "type", "geometry", "anomaly_index"]
+            result['geometry'] = result.geometry.apply(lambda x: wkb.loads(bytes(x)))
+            result = result.sort_values('anomaly_index', axis=0, ascending=False)
+
+        else:
+            query = session.query(FlaringFacility)
+
+            if facility_id is not None:
+                query = query.filter(FlaringFacility.id.in_(to_list(facility_id)))
+
+            result = pd.read_sql(query.statement, session.bind)
+            result = update_geometry_from_wkb(result, to='shape')
+
+        response = self.build_response(result=result,
+                                     format=format,
+                                     nest_in_data=nest_in_data,
+                                     download=download)
+        return response
+
+    def build_response(self, result, format, nest_in_data, download):
+
+        result.replace({np.nan: None}, inplace=True)
+        if format == "csv":
+            result.drop('geometry', axis=1, inplace=True)
+            return Response(
+                response=result.to_csv(index=False),
+                mimetype="text/csv",
+                headers={"Content-disposition":
+                             "attachment; filename=flaringfacility.csv"})
+
+        if format == "json":
+            result.drop('geometry', axis=1, inplace=True)
+            if nest_in_data:
+                resp_content = json.dumps({"data": result.to_dict(orient="records")}, cls=JsonEncoder)
+            else:
+                resp_content = json.dumps(result.to_dict(orient="records"), cls=JsonEncoder)
+
+            return Response(
+                response=resp_content,
+                status=200,
+                mimetype='application/json')
+
+        if format == "geojson":
+            berths_gdf = gpd.GeoDataFrame(result, geometry='geometry')
+            berths_geojson = berths_gdf.to_json(cls=JsonEncoder)
+
+            if nest_in_data:
+                resp_content = '{"data": ' + berths_geojson + '}'
+            else:
+                resp_content = berths_geojson
+
+            if download:
+                headers = {"Content-disposition":
+                               "attachment; filename=flaringfacility.geojson"}
+            else:
+                headers = {}
+
+            return Response(
+                response=resp_content,
+                status=200,
+                mimetype='application/json',
+                headers=headers)
+
+
+
 @routes_api.route('/v0/flaring', strict_slashes=False)
-class DepartureResource(Resource):
+class FlaringResource(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('facility_id', help='facility id(s)',
@@ -28,7 +131,7 @@ class DepartureResource(Resource):
                         help='rolling average window (in days). Default: no rolling averaging',
                         required=False, default=None)
     parser.add_argument('date_from', help='start date for arrival (format 2020-01-15)',
-                        default="2022-01-01", required=False)
+                        default="2018-01-01", required=False)
     parser.add_argument('date_to', type=str, help='end date for arrival (format 2020-01-15)', required=False,
                         default=dt.datetime.today().strftime("%Y-%m-%d"))
     parser.add_argument('nest_in_data', help='Whether to nest the geojson content in a data key.',
@@ -41,7 +144,7 @@ class DepartureResource(Resource):
     @routes_api.expect(parser)
     def get(self):
 
-        params = DepartureResource.parser.parse_args()
+        params = FlaringResource.parser.parse_args()
         facility_id = params.get("facility_id")
         format = params.get("format")
         date_from = params.get("date_from")
@@ -50,7 +153,9 @@ class DepartureResource(Resource):
         rolling_days = params.get("rolling_days")
         download = params.get("download")
 
-        query = session.query(FlaringFacility,
+        query = session.query(FlaringFacility.id,
+                              FlaringFacility.name,
+                              FlaringFacility.type,
                               Flaring.date,
                               Flaring.value) \
                 .join(Flaring, FlaringFacility.id == Flaring.facility_id)
@@ -78,7 +183,6 @@ class DepartureResource(Resource):
 
         result.replace({np.nan: None}, inplace=True)
         if format == "csv":
-            result.drop('geometry', axis=1, inplace=True)
             return Response(
                 response=result.to_csv(index=False),
                 mimetype="text/csv",
@@ -86,7 +190,6 @@ class DepartureResource(Resource):
                              "attachment; filename=flaring.csv"})
 
         if format == "json":
-            result.drop('geometry', axis=1, inplace=True)
             if nest_in_data:
                 resp_content = json.dumps({"data": result.to_dict(orient="records")}, cls=JsonEncoder)
             else:
@@ -124,6 +227,7 @@ class DepartureResource(Resource):
     def roll_average(self, result, rolling_days):
 
         if rolling_days is not None:
+            result['date'] = pd.to_datetime(result.date)
             date_column = "date"
             value_columns = ['value']
             min_date = result[date_column].min()
@@ -134,7 +238,7 @@ class DepartureResource(Resource):
             result = result \
                 .groupby([x for x in result.columns if x not in ([date_column] + value_columns)]) \
                 .apply(lambda x: x.set_index(date_column) \
-                       .resample("D").sum() \
+                       .resample("D")[value_columns].sum() \
                        .reindex(daterange) \
                        .fillna(0) \
                        .rolling(rolling_days, min_periods=rolling_days) \
--- engine/tests/test_flaring.py
+++ engine/tests/test_flaring.py
@@ -5,7 +5,7 @@ from base.db import init_db
 
 
 def test_flaring(app):
-    # init_db(drop_first=False)
+    init_db(drop_first=False)
     update()
     # get_fields()
     # download_nvf_date('2022-01-01')
