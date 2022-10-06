8449fb942d5e31121bcf3e09839649773032bbd9
lietava-git <lietava.jan@gmail.com>
2022-10-06 08:19:54 +0100

    - sts testing updated

--- engine/tests/test_sts.py
+++ engine/tests/test_sts.py
@@ -1,6 +1,17 @@
 from base.db import session, check_if_table_exists
 from base.models import STSLocation, ShipmentArrivalLocationSTS, ShipmentDepartureLocationSTS
 from engine.sts import fill, generate_geojson, detect_sts_departure_location, detect_sts_arrival_location
+from engine import shipment, arrival
+from engine.marinetraffic import Marinetraffic
+from engine.mtevents import back_fill_ship_position
+
+def test_marinetraffic_get_position():
+    #Marinetraffic.get_positions('9247481', date_from= '2022-07-01 11:30' , date_to= '2022-07-01 11:35', period='hourly')
+    #d = Marinetraffic.get_closest_position(imo='9247481', date='2022-07-01 11:30')
+    #assert d is not None
+    back_fill_ship_position()
+def test_update_shipment():
+    shipment.update("2021-01-01")
 
 def test_table_exists():
     check_if_table_exists(STSLocation, create_table=True)
