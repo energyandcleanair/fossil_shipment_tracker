a25b1d8658fc9578e5e17307abac71ccfc986238
lietava-git <lietava.jan@gmail.com>
2022-12-13 11:08:57 +0000

    - added diagnostic chart sending to slack as part of shipment update

--- engine/shipment.py
+++ engine/shipment.py
@@ -37,6 +37,8 @@ def update(date_from="2021-01-01"):
                                       "date_utc >= '%s'" % (to_datetime(date_from).strftime('%Y-%m-%d')))
     execute_statement(sql_content, print_result=True, slack_result=True)
 
+    send_diagnostic_chart()
+
 
     # # Create a shipment for each dangling departure
     # # and complete it for each arrival
