6a9da48db180324e3bee33c1813d380d0f4d128e
Hubert Thieriot <>
2023-02-21 18:32:21 +0800

    -adding a created_at for ship_insurers, for later debugs

--- base/models/models.py
+++ base/models/models.py
@@ -424,7 +424,8 @@ class ShipInsurer(Base):
         ForeignKey(DB_TABLE_COMPANY + ".id", onupdate="CASCADE"),
         nullable=False,
     )  # Link to cleaned list of companies
-    updated_on = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+    updated_on = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime, server_default=func.now())
 
     __tablename__ = DB_TABLE_SHIP_INSURER
     __table_args__ = (
