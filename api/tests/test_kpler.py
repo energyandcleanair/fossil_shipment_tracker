526e267d9c6daf56d316f9f3370e933b3b13f9c9
lietava-git <lietava.jan@gmail.com>
2023-03-02 15:45:07 +0000

    - added test for kpler pricing

--- api/tests/test_kpler.py
+++ api/tests/test_kpler.py
@@ -14,3 +14,18 @@ def test_kpler(app):
         data_df = pd.DataFrame(data)
         expected_columns = set(["origin_iso2", "destination_iso2", "product", "date"])
         assert set(data_df.columns) >= expected_columns
+
+
+def test_kpler_pricing(app):
+    # Create a test client using the Flask application configured for testing
+    with app.test_client() as test_client:
+        params = {"format": "json", "origin_iso2": "RU", "destination_iso2": "SE", "pricing_scenario": "default"}
+        response = test_client.get("/v0/kpler_flow?" + urllib.parse.urlencode(params))
+        assert response.status_code == 200
+        data = response.json["data"]
+        assert len(data) >= 4
+        data_df = pd.DataFrame(data)
+        expected_columns = set(["origin_iso2", "destination_iso2", "product", "date", "value_eur"])
+        assert set(data_df.columns) >= expected_columns
+        # check pricing was found for these shipments
+        assert data_df[data_df['value_eur'].isnull()].empty
