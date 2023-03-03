11c0c83e8253eafb7f4b60df204d4dde782f9e7d
lietava-git <lietava.jan@gmail.com>
2023-03-03 16:51:49 +0000

    - adding specific test examples/samples to reduce test time

--- engine/tests/test_sts.py
+++ engine/tests/test_sts.py
@@ -1,3 +1,5 @@
+import sqlalchemy.sql.functions
+
 from base.db import session, check_if_table_exists
 from base.models import STSLocation, ShipmentArrivalLocationSTS, ShipmentDepartureLocationSTS, ShipmentWithSTS
 from engine.sts import fill, generate_geojson, detect_sts_departure_location, detect_sts_arrival_location
@@ -8,33 +10,28 @@ import datetime as dt
 
 
 def test_find_multistage_sts():
-    sts.check_multi_stage_sts(ship_imo='9357638')
+    sts.check_multi_stage_sts(ship_imo='9762986')
+
 
 def test_find_portcall():
-    sts.fill_portcalls_around_sts()
+    sts.fill_portcalls_around_sts(ship_imo='9762986')
 
 
 def test_arrival_sts_shipment_update():
-    sts_shipment_ids = [s.id for s in session.query(ShipmentWithSTS.id).all()]
+    sts_shipment_ids = [s.id for s in
+                        session.query(
+                            ShipmentWithSTS.id
+                        )
+                        .filter(ShipmentWithSTS.status == "ongoing")
+                        .order_by(sqlalchemy.sql.functions.random())
+                        .limit(2)
+                        .all()]
 
     arrival.update(date_from='2021-09-01', date_to='2023-01-01', shipment_id=sts_shipment_ids)
 
 
 def test_marinetraffic_get_position():
-    # Marinetraffic.get_positions('9247481', date_from= '2022-07-01 11:30' , date_to= '2022-07-01 11:35', period='hourly')
-    # d = Marinetraffic.get_closest_position(imo='9247481', date='2022-07-01 11:30')
-    # assert d is not None
-    back_fill_ship_position()
-
-
-def test_update_shipment():
-    shipment.update("2021-01-01")
-
-
-def test_table_exists():
-    check_if_table_exists(STSLocation, create_table=True)
-    check_if_table_exists(ShipmentDepartureLocationSTS, create_table=True)
-    check_if_table_exists(ShipmentArrivalLocationSTS, create_table=True)
+    back_fill_ship_position(event_id='3179')
 
 
 def test_generate_geojson():
