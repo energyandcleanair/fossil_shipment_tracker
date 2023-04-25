deabd68007469a3059a271b0f341de64ca69ac9c
hubert-crea <hubert@energyandcleanair.org>
2023-04-25 14:16:06 +0800

    -decommenting upload

--- engine/kpler_scraper/update.py
+++ engine/kpler_scraper/update.py
@@ -289,8 +289,8 @@ def update_flows_reverse(
                             )
                             if df is not None:
                                 df_zones.append(df)
-                            # if not add_unknown_only:
-                            # upload_flows(df, ignore_if_copy_failed=ignore_if_copy_failed)
+                            if not add_unknown_only:
+                                upload_flows(df, ignore_if_copy_failed=ignore_if_copy_failed)
 
                         if add_unknown and len(df_zones) > 0:
                             # Add an unknown one
