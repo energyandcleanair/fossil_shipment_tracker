48c1d0e881156237c46d368b8a74b797cd3c0ffe
hubert-crea <hubert@energyandcleanair.org>
2023-05-30 10:12:19 +0100

    -using call-based for departure as well

--- update_main.py
+++ update_main.py
@@ -26,33 +26,37 @@ import datetime as dt
 
 
 def update():
-    #integrity.check()
-    #sts.check_multi_stage_sts()
-    portcall.update_departures(departure_port_iso2=['RU'],
-                               date_from=-14,
-                               force_rebuild=True,
-                               between_existing_only=True)
+    # integrity.check()
+    # sts.check_multi_stage_sts()
+    portcall.update_departures(
+        departure_port_iso2=["RU"],
+        date_from=-14,
+        force_rebuild=True,
+        between_existing_only=True,
+        use_call_based=True,
+    )
     ship.update()
     departure.update()
-    arrival.update(date_from=dt.date.today() - dt.timedelta(days=90), departure_port_iso2=['RU'])
-    #currency.update()
-    #company.update()
-    #mtevents.update(date_from=dt.date.today() - dt.timedelta(days=90), only_for_ongoing_shipments=False)
-    #sts.update(date_from=dt.date.today() - dt.timedelta(days=90))
-    #sts.check_multi_stage_sts("2022-06-01")
+    arrival.update(date_from=dt.date.today() - dt.timedelta(days=90), departure_port_iso2=["RU"])
+    # currency.update()
+    # company.update()
+    # mtevents.update(date_from=dt.date.today() - dt.timedelta(days=90), only_for_ongoing_shipments=False)
+    # sts.update(date_from=dt.date.today() - dt.timedelta(days=90))
+    # sts.check_multi_stage_sts("2022-06-01")
     shipment.update()
     position.update(date_from=dt.date.today() - dt.timedelta(days=90))
-    #destination.update()
-    #berth.update()
-    #entsog.update(date_from=-21, nodata_error_date_from=-4)
-    #rscript.update()
-    #trajectory.update()
-    #flaring.update()
+    # destination.update()
+    # berth.update()
+    # entsog.update(date_from=-21, nodata_error_date_from=-4)
+    # rscript.update()
+    # trajectory.update()
+    # flaring.update()
     alert.update()
     counter.update()
     backuper.update()
-    #integrity.check()
+    # integrity.check()
     return
+
 
 if __name__ == "__main__":
     print("=== Using %s environment ===" % (base.db.environment,))
