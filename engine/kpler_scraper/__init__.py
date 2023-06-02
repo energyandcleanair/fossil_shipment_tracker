ae293f46ddd8002547af26af53ff4f15d2dc4340
hubert-crea <hubert@energyandcleanair.org>
2023-06-02 16:10:55 +0100

    -kpler flows are changing overtime in kpler database itself. We need to account for potential change of commodities

--- engine/kpler_scraper/__init__.py
+++ engine/kpler_scraper/__init__.py
@@ -1,3 +1,3 @@
 from .scraper import KplerScraper
-from .update import update_flows, update_flows_reverse
+from .update import update_flows, update_flows_reverse, update_is_valid
 from .misc import *
