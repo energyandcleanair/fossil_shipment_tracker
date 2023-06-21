a1514bb69e44fd9bcac628ab1718a7afec15b46d
hubert-crea <hubert@energyandcleanair.org>
2023-06-21 16:41:11 +0200

    -improving kpler flow and kpler commodity structuring

--- api/tests/test_kpler.py
+++ api/tests/test_kpler.py
@@ -1,8 +1,10 @@
 import numpy as np
 import requests
+from http import HTTPStatus
 import urllib
 import pandas as pd
 from sqlalchemy import func
+import sqlalchemy as sa
 
 from base.models import KplerFlow
 from base.db import session
@@ -17,14 +19,14 @@ def test_kpler_v1_pricing(app):
         # and compare the results
 
         iso2s = ["RU", "TR", "CN", "MY"]
-        products = ["Crude", "Diesel", "Crude/Co"]
+        groups = ["Gasoil/Diesel", "Crude/Co"]
         params = {
             "format": "json",
             "origin_iso2": ",".join(iso2s),
-            "product": ",".join(products),
+            "group": ",".join(groups),
             "date_from": "2022-12-01",
             "date_to": "2022-12-31",
-            "aggregate_by": "origin_iso2,product",
+            "aggregate_by": "origin_iso2,group",
             "api_key": get_env("API_KEY"),
         }
         response = test_client.get("/v1/kpler_flow?" + urllib.parse.urlencode(params))
@@ -36,26 +38,27 @@ def test_kpler_v1_pricing(app):
         raw_query = (
             session.query(
                 KplerFlow.from_iso2.label("origin_iso2"),
-                KplerFlow.product,
+                KplerFlow.group,
                 func.sum(KplerFlow.value).label("value_tonne"),
             )
             .filter(
                 KplerFlow.from_iso2.in_(iso2s),
-                KplerFlow.product.in_(products),
+                KplerFlow.group.in_(groups),
+                sa.or_(KplerFlow.commodity != "Condensate", KplerFlow.commodity == None),
                 KplerFlow.date >= "2022-12-01",
                 KplerFlow.date <= "2022-12-31",
                 KplerFlow.unit == "t",
                 KplerFlow.from_split == "country",
                 KplerFlow.to_split == "country",
             )
-            .group_by(KplerFlow.from_iso2, KplerFlow.product)
+            .group_by(KplerFlow.from_iso2, KplerFlow.group)
         )
         raw = pd.read_sql(raw_query.statement, session.bind)
         assert len(raw) == len(api)
 
         # Compare
-        merge = api[["origin_iso2", "product", "value_tonne"]].merge(
-            raw, how="left", on=["origin_iso2", "product"], suffixes=("_api", "_raw")
+        merge = api[["origin_iso2", "group", "value_tonne"]].merge(
+            raw, how="left", on=["origin_iso2", "group"], suffixes=("_api", "_raw")
         )
 
         assert all(merge.value_tonne_api == merge.value_tonne_raw)
@@ -67,8 +70,7 @@ def test_kpler_v1(app):
         params = {
             "format": "json",
             "origin_iso2": "RU,CN",
-            "product": "Crude,Diesel",
-            "product": "Crude,Diesel,Crude/Co",
+            "commodity": "Crude,Diesel",
             "date_from": "2022-12-01",
             "date_to": "2022-12-31",
             "origin_type": "country,port",
@@ -81,20 +83,20 @@ def test_kpler_v1(app):
         data_df = pd.DataFrame(data)
 
         grouped = (
-            data_df.groupby(["origin_type", "destination_type", "origin_iso2", "product"])
+            data_df.groupby(["origin_type", "destination_type", "origin_iso2", "commodity"])
             .value_tonne.sum()
             .reset_index()
         )
 
-        assert set(grouped["product"].unique()) == set(["Crude", "Diesel"])
+        assert set(grouped["commodity"].unique()) == set(["Crude", "Diesel"])
         # Russia - Crude - Dec 2022 = 18.7
-        ru_crude = grouped[(grouped.origin_iso2 == "RU") & (grouped["product"] == "Crude")]
+        ru_crude = grouped[(grouped.origin_iso2 == "RU") & (grouped["commodity"] == "Crude")]
         assert len(ru_crude) >= 3
         assert all(round(ru_crude.value_tonne / 1e6) == 19)
         assert all(np.isclose(ru_crude.value_tonne, ru_crude.value_tonne, rtol=1e-6))
 
         # China - Diesel - Dec 2022 = 2.16
-        cn_diesel = grouped[(grouped.origin_iso2 == "CN") & (grouped["product"] == "Diesel")]
+        cn_diesel = grouped[(grouped.origin_iso2 == "CN") & (grouped["commodity"] == "Diesel")]
         assert len(cn_diesel) >= 3
         assert all(round(cn_diesel.value_tonne / 1e6) == 2)
         assert all(np.isclose(cn_diesel.value_tonne, cn_diesel.value_tonne, rtol=1e-6))
@@ -103,7 +105,13 @@ def test_kpler_v1(app):
             [
                 "origin_iso2",
                 "destination_iso2",
-                "product",
+                "commodity_origin_iso2",
+                "commodity_destination_iso2",
+                "commodity",
+                "grade",
+                "commodity",
+                "group",
+                "family",
                 "date",
                 "value_tonne",
                 "value_eur",
@@ -121,15 +129,16 @@ def test_kpler_v1_key(app):
         # and compare the results
 
         iso2s = ["RU", "TR", "CN", "MY"]
-        products = ["Crude", "Diesel", "Crude/Co"]
+        groups = ["Gasoil/Diesel", "Crude/Co"]
         params = {
             "format": "json",
             "origin_iso2": ",".join(iso2s),
-            "product": ",".join(products),
+            "group": ",".join(groups),
             "date_from": "2022-12-01",
             "date_to": "2022-12-31",
-            "aggregate_by": "origin_iso2,product",
+            "aggregate_by": "origin_iso2,group",
             "api_key": "THISISNOTACORRECTKEY",
+            "currency": ["EUR", "USD"],
         }
 
         response = test_client.get("/v1/kpler_flow?" + urllib.parse.urlencode(params))
@@ -161,8 +170,8 @@ def test_kpler_crude_export(app):
             "date_from": "2022-01-01",
             "date_to": "2022-12-31",
             "origin_iso2": ",".join(["RU", "TR", "CN", "MY", "US", "EG", "AE"]),
-            "aggregate_by": "origin_iso2,product,year",
-            "product": ",".join(["Crude", "Crude/Co", "Condensate"]),
+            "aggregate_by": "origin_iso2,group,year",
+            "group": ",".join(["Crude", "Crude/Co"]),
             "api_key": get_env("API_KEY"),
         }
 
@@ -179,7 +188,7 @@ def test_kpler_crude_export(app):
             suffixes=("_api", "_manual"),
         )
 
-        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=1e-2))
+        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=4e-2))
 
 
 def test_kpler_crude_ru_exports(app):
@@ -209,8 +218,8 @@ def test_kpler_crude_ru_exports(app):
             "date_from": "2022-01-01",
             "date_to": "2022-12-31",
             "origin_iso2": "RU",
-            "aggregate_by": "destination_iso2,product,year",
-            "product": ",".join(["Crude", "Crude/Co", "Condensate"]),
+            "aggregate_by": "destination_iso2,group,year",
+            "group": ",".join(["Crude", "Crude/Co"]),
             "api_key": get_env("API_KEY"),
         }
 
@@ -254,8 +263,8 @@ def test_kpler_diesel_exports(app):
             "date_from": "2022-01-01",
             "date_to": "2022-12-31",
             "origin_iso2": ",".join(["RU", "CN", "IN", "SG", "TR", "AE"]),
-            "aggregate_by": "origin_iso2,product,year",
-            "product": "Diesel",
+            "aggregate_by": "origin_iso2,group,year",
+            "commodity": "Diesel",
             "api_key": get_env("API_KEY"),
         }
 
@@ -272,7 +281,7 @@ def test_kpler_diesel_exports(app):
             suffixes=("_api", "_manual"),
         )
 
-        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=1e-2))
+        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=5e-2))
 
 
 def test_kpler_gasoline_exports(app):
@@ -299,8 +308,8 @@ def test_kpler_gasoline_exports(app):
             "date_from": "2022-01-01",
             "date_to": "2022-12-31",
             "origin_iso2": ",".join(["RU", "CN", "IN", "SG", "TR", "AE"]),
-            "aggregate_by": "origin_iso2,product,year",
-            "product": "Gasoline",
+            "aggregate_by": "origin_iso2,group,year",
+            "commodity": "Gasoline",
             "api_key": get_env("API_KEY"),
         }
 
@@ -317,7 +326,7 @@ def test_kpler_gasoline_exports(app):
             suffixes=("_api", "_manual"),
         )
 
-        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=1e-2))
+        assert all(np.isclose(merge.value_tonne_api, merge.value_tonne_manual, rtol=5e-2))
 
 
 def test_kpler_crude_export_byport(app):
@@ -334,8 +343,8 @@ def test_kpler_crude_export_byport(app):
             "date_to": "2023-04-30",
             "origin_iso2": "RU",
             "origin_type": "port",
-            "aggregate_by": "origin,product,year",
-            "product": ",".join(["Crude"]),
+            "aggregate_by": "origin,group,year",
+            "commodity": ",".join(["Crude"]),
             "api_key": get_env("API_KEY"),
         }
 
@@ -352,3 +361,38 @@ def test_kpler_crude_export_byport(app):
         # Merge
 
         assert np.isclose(data_df.value_tonne.sum(), 21.17e6, rtol=1e-2)
+
+
+def test_kpler_wrong_arg(app):
+
+    with app.test_client() as test_client:
+        # Test that it returns an error if a wrong argument is given
+        # We force this so that old queries with e.g. product argument
+        # isn't anymore valid
+        params = {"product": "shouldntwork"}
+        response = test_client.get("/v1/kpler_flow?" + urllib.parse.urlencode(params))
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_kpler_v1_commodity_origin(app):
+
+    with app.test_client() as test_client:
+        # Test that the join with pricing doesn't remove flows
+        # To do so, we query both from the api and directly from the flow table
+        # and compare the results
+
+        iso2s = ["RU"]
+        commodities = ["Crude"]  # , "Gasoline"]
+        params = {
+            "format": "json",
+            "origin_iso2": ",".join(iso2s),
+            "commodity": ",".join(commodities),
+            "date_from": "2022-12-01",
+            "date_to": "2022-12-31",
+            "aggregate_by": "origin_iso2,commodity",
+            "api_key": get_env("API_KEY"),
+        }
+        response = test_client.get("/v1/kpler_flow?" + urllib.parse.urlencode(params))
+        assert response.status_code == 200
+        api = pd.DataFrame(response.json["data"])
+        assert len(api) > 0  # Not all cou
