1cb07c4f6b240d410cd03ca9a41222458372fe28
hubert-crea <hubert@energyandcleanair.org>
2023-07-31 09:17:58 +0200

    -creating diagnostics folder

--- engine/shipment.py
+++ engine/shipment.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 import seaborn as sns
 import tempfile
+import os
 
 from engine import departure
 from base.db_utils import execute_statement
@@ -13,9 +14,10 @@ from slack_sdk.errors import SlackApiError
 from base.models import Shipment, ShipmentWithSTS
 from base import PRICING_DEFAULT
 
+
 def rebuild(date_from="2021-01-01"):
     logger_slack.info("=== Shipment rebuild ===")
-    with open('engine/shipment_rebuild.sql', 'r') as file:
+    with open("engine/shipment_rebuild.sql", "r") as file:
         sql_rebuild = file.read()
 
     execute_statement(sql_rebuild, print_result=True)
@@ -27,18 +29,19 @@ def rebuild(date_from="2021-01-01"):
 def update(date_from="2021-01-01"):
     logger_slack.info("=== Shipment update ===")
 
-    with open('engine/shipment_refresh_sts.sql', 'r') as file:
+    with open("engine/shipment_refresh_sts.sql", "r") as file:
         sql_content = file.read()
 
-    with open('engine/shipment_refresh.sql', 'r') as file:
+    with open("engine/shipment_refresh.sql", "r") as file:
         sql_content += file.read()
 
-    sql_content = sql_content.replace("date_utc >= '2021-01-01'",
-                                      "date_utc >= '%s'" % (to_datetime(date_from).strftime('%Y-%m-%d')))
+    sql_content = sql_content.replace(
+        "date_utc >= '2021-01-01'",
+        "date_utc >= '%s'" % (to_datetime(date_from).strftime("%Y-%m-%d")),
+    )
     execute_statement(sql_content, print_result=True, slack_result=True)
 
     send_diagnostic_chart()
-
 
     # # Create a shipment for each dangling departure
     # # and complete it for each arrival
@@ -73,6 +76,7 @@ def update(date_from="2021-01-01"):
     #     session.add(new_shipment)
     # session.commit()
 
+
 def return_combined_shipments(session, columns=None):
     """
     Combine sts and non sts shipment tables and return the union subquery
@@ -84,11 +88,9 @@ def return_combined_shipments(session, columns=None):
     if not columns:
         columns = [column.name for column in inspect(Shipment).c]
 
-    non_sts_shipments = session.query(Shipment) \
-        .options(load_only(*columns))
+    non_sts_shipments = session.query(Shipment).options(load_only(*columns))
 
-    sts_shipments = session.query(ShipmentWithSTS) \
-        .options(load_only(*columns))
+    sts_shipments = session.query(ShipmentWithSTS).options(load_only(*columns))
 
     return non_sts_shipments.union(sts_shipments).subquery()
 
@@ -96,43 +98,47 @@ def return_combined_shipments(session, columns=None):
 def send_diagnostic_chart():
     from api.routes.voyage import VoyageResource
 
-    params = {'aggregate_by': ['departure_date', 'status', 'commodity'],
-              'commodity': ['crude_oil', 'oil_products', 'lng', 'coal'],
-              'departure_iso2': 'RU',
-              'departure_date_from': '2022-01-01',
-              'rolling_days': 14,
-              'currency': 'EUR',
-              'pricing_scenario' : [PRICING_DEFAULT],
-              'commodity_grouping': 'default'
-              }
+    params = {
+        "aggregate_by": ["departure_date", "status", "commodity"],
+        "commodity": ["crude_oil", "oil_products", "lng", "coal"],
+        "departure_iso2": "RU",
+        "departure_date_from": "2022-01-01",
+        "rolling_days": 14,
+        "currency": "EUR",
+        "pricing_scenario": [PRICING_DEFAULT],
+        "commodity_grouping": "default",
+    }
 
     v = VoyageResource().get_from_params(params=params)
     v = pd.DataFrame(json.loads(v.response[0]))
 
-    v['value_tonne'] = pd.to_numeric(v.value_tonne)
-    v['departure_date'] = pd.to_datetime(v.departure_date)
+    v["value_tonne"] = pd.to_numeric(v.value_tonne)
+    v["departure_date"] = pd.to_datetime(v.departure_date)
 
-    shipment_diagnostic = sns.relplot(data=v,
-                kind='line',
-                x='departure_date',
-                y='value_tonne',
-                hue='status',
-                errorbar=None,
-                col="commodity",
-                col_wrap=2,
-                facet_kws={'sharey': False, 'sharex': True},
-                height=8)
+    shipment_diagnostic = sns.relplot(
+        data=v,
+        kind="line",
+        x="departure_date",
+        y="value_tonne",
+        hue="status",
+        errorbar=None,
+        col="commodity",
+        col_wrap=2,
+        facet_kws={"sharey": False, "sharex": True},
+        height=8,
+    )
 
-    shipment_diagnostic.figure.savefig('./misc/diagnostics/shipment_diagnostics.png')
+    folder = "misc/diagnostics/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    shipment_diagnostic.figure.savefig(os.path.join(folder, "shipment_diagnostics.png"))
 
     try:
-        filepath = './misc/diagnostics/shipment_diagnostics.png'
-        response = slacker.files_upload(channels='#log-russia-counter', file=filepath)
+        filepath = "./misc/diagnostics/shipment_diagnostics.png"
+        response = slacker.files_upload(channels="#log-russia-counter", file=filepath)
         assert response["file"]  # the uploaded file
     except SlackApiError as e:
         # You will get a SlackApiError if "ok" is False
         assert e.response["ok"] is False
         assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
         logger_slack.error(f"Got an error: {e.response['error']}")
-
-
