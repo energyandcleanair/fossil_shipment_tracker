6ee3d6a4391608f57c5d563d14c10dccfedeafca
hubert-crea <hubert@energyandcleanair.org>
2023-10-02 18:00:04 +0100

    -better validity setting

--- engine/kpler_scraper/update_is_valid.sql
+++ engine/kpler_scraper/update_is_valid.sql
@@ -117,17 +117,32 @@ and kpler_trade.departure_date_utc = last_update.departure_date_utc
 and kpler_trade.departure_zone_id = last_update.departure_zone_id;
 
 
-with last_update as (
-	select departure_zone_id, arrival_zone_id, vessel_imos, max(updated_on) as updated_on_max
-    from kpler_trade
-    group by 1, 2, 3
+-- This one is to remove old partial updates, can be quite 'aggressive'
+-- Theroretically should fix all of them
+WITH last_update AS (
+    SELECT
+        sub.origin_date,
+        sub.country_iso2_departure,
+        sub.product_id,
+        MAX(sub.updated_on_max) AS updated_on_max
+    FROM (
+        SELECT
+            DATE(kt.departure_date_utc) AS origin_date,
+            kz.country_iso2 AS country_iso2_departure,
+            kt.product_id,
+            MAX(kt.updated_on) OVER (PARTITION BY kz.country_iso2, kt.product_id
+                                    ORDER BY DATE(kt.departure_date_utc), kt.updated_on
+                                    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS updated_on_max
+        FROM kpler_trade AS kt
+        JOIN kpler_zone AS kz ON kt.departure_zone_id = kz.id
+    ) AS sub
+    GROUP BY sub.origin_date, sub.country_iso2_departure, sub.product_id
 )
 
-update kpler_trade
-set is_valid = (is_valid) and (updated_on_max - updated_on < '2 hours')
-from last_update
-where kpler_trade.departure_zone_id = last_update.departure_zone_id
-and kpler_trade.arrival_zone_id = last_update.arrival_zone_id
-and kpler_trade.vessel_imos = last_update.vessel_imos;
-
-COMMIT;
+UPDATE kpler_trade AS kt
+SET is_valid = (kt.is_valid) AND (lu.updated_on_max - kt.updated_on < interval '7 days')
+FROM last_update AS lu, kpler_zone AS kz
+WHERE kt.departure_zone_id = kz.id
+AND kz.country_iso2 = lu.country_iso2_departure
+AND kt.product_id = lu.product_id
+AND DATE(kt.departure_date_utc) = lu.origin_date;
