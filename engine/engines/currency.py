c4a96f204f9a0073ceac20ad8558962d8be54809
Panda Rushwood <panda@energyandcleanair.org>
2024-04-22 11:54:20 +0100

    Add replacement currency library

--- engine/engines/currency.py
+++ engine/engines/currency.py
@@ -1,3 +1,4 @@
+from itertools import product
 import pandas as pd
 import datetime as dt
 import geopandas as gpd
@@ -14,9 +15,9 @@ from base.models import Country, Currency
 from base.models import DB_TABLE_CURRENCY
 import base
 
+from currency_converter import CurrencyConverter, ECB_URL
 
-from forex_python.converter import CurrencyRates
-from forex_python.converter import RatesNotAvailableError
+from urllib.request import urlretrieve
 
 
 def update(date_from=dt.date(2022, 1, 1), date_to=None, force=False):
@@ -25,57 +26,19 @@ def update(date_from=dt.date(2022, 1, 1), date_to=None, force=False):
     :return:
     """
 
+    date_from = to_datetime(choose_date_from(date_from, force))
+    date_to = to_datetime(date_to) or dt.date.today() - dt.timedelta(days=1)
+
     logger_slack.info("=== Currency update ===")
 
     try:
 
-        c = CurrencyRates()
+        converter = build_converter()
 
-        last_date = (
-            session.query(func.max(Currency.date)).filter(sa.not_(Currency.estimated)).first()[0]
-        )
-        if last_date and not force:
-            date_from = max(date_from, last_date)
+        currencies = get_currency_names(converter, date_to)
+        dates = pd.date_range(start=date_from, end=date_to)
 
-        tqdm.pandas()
-
-        date_to = to_datetime(date_to) or dt.date.today() - dt.timedelta(days=1)
-
-        dates = pd.date_range(start=to_datetime(date_from), end=date_to)
-        exchanges = pd.DataFrame(data={"date": dates})
-        try:
-            exchanges["per_eur"] = exchanges.progress_apply(
-                lambda row: c.get_rates("EUR", row.date), axis=1
-            )
-            exchanges = pd.concat(
-                [exchanges.drop(["per_eur"], axis=1), pd.json_normalize(exchanges["per_eur"])],
-                axis=1,
-            )
-
-            exchanges["EUR"] = 1
-            # Fill missing values
-            exchanges.fillna(method="ffill", inplace=True)
-            exchanges.fillna(method="bfill", inplace=True)
-            exchanges = pd.melt(
-                exchanges, id_vars="date", var_name="currency", value_name="per_eur"
-            )
-            exchanges["estimated"] = False
-
-        except (RatesNotAvailableError, ConnectionError):
-            # Prolong historical value
-            last = session.query(Currency).filter(
-                Currency.date == last_date, sa.not_(Currency.estimated)
-            )
-            exchanges = pd.read_sql(last.statement, session.bind).drop("id", axis=1)
-            exchanges["date"] = pd.to_datetime(exchanges.date)
-
-            currencies = exchanges.currency.unique()
-            idx = pd.MultiIndex.from_product((dates, currencies), names=["date", "currency"])
-            exchanges = exchanges.set_index(["date", "currency"]).reindex(idx).reset_index()
-            exchanges["per_eur"] = exchanges.groupby("currency")["per_eur"].transform(
-                lambda v: v.ffill()
-            )
-            exchanges["estimated"] = pd.isna(exchanges.estimated)
+        exchanges = get_exchange_rates(converter, currencies, dates)
 
         upsert(exchanges, DB_TABLE_CURRENCY, "unique_currency")
         session.commit()
@@ -86,3 +49,46 @@ def update(date_from=dt.date(2022, 1, 1), date_to=None, force=False):
             stack_info=True,
             exc_info=True,
         )
+
+
+def get_exchange_rates(converter, currencies, dates):
+    exchanges = pd.DataFrame(list(product(dates, currencies)), columns=["date", "currency"])
+
+    exchanges["per_eur"] = exchanges.progress_apply(
+        lambda row: converter.convert(
+            amount=1, currency="EUR", new_currency=row.currency, date=row.date
+        ),
+        axis=1,
+    )
+    exchanges["estimated"] = False
+    return exchanges
+
+
+def get_currency_names(converter, date_to):
+    currencies = [
+        currency
+        for (currency, bounds) in converter.bounds.items()
+        if bounds.last_date > date_to - dt.timedelta(days=7)
+    ]
+
+    return currencies
+
+
+def choose_date_from(date_from, force):
+    last_date = (
+        session.query(func.max(Currency.date)).filter(sa.not_(Currency.estimated)).first()[0]
+    )
+    if last_date and not force:
+        date_from = max(date_from, last_date)
+    return date_from
+
+
+def build_converter():
+    filename = "cache/ecb.zip"
+    urlretrieve(ECB_URL, filename)
+
+    converter = CurrencyConverter(
+        filename, fallback_on_missing_rate=True, fallback_on_wrong_date=True
+    )
+
+    return converter
---
+++ engine/run/update_currency.py
@@ -0,0 +1,36 @@
+from engines import (
+    commodity,
+    company,
+    engine_r,
+    trajectory,
+    position,
+    destination,
+    berth,
+    currency,
+    counter,
+    entsog,
+    flaring,
+    kpler_scraper,
+    kpler_trade_computed,
+)
+
+import integrity
+import base
+from base.logger import logger_slack
+
+import datetime as dt
+
+
+def update():
+    currency.update()
+    return
+
+
+if __name__ == "__main__":
+    logger_slack.info("=== Lite update: using %s environment ===" % (base.db.environment,))
+    try:
+        update()
+        logger_slack.info("=== Lite update complete ===")
+    except BaseException as e:
+        logger_slack.error("=== Lite update failed", stack_info=True, exc_info=True)
+        raise e
